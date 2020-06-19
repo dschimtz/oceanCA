119cdd8f50426740fa69ad2d081a3bebc32ce72c
Dennis Schimtzek <dschimtz@ad.unc.edu>
2020-06-19 15:01:48 -0400

    some work done

--- cell.py
+++ cell.py
@@ -1,12 +1,49 @@
-class Cell:
+import tkinter as tk
 
-	# rgb should be inputted 
-	def __init__(self, x1, y1, x2, y2, rgb):
+
+class Cell:
+	"""
+	Cell
+
+	...
+
+	Attributes
+	----------
+	coords : int
+		includes: x1, y1, x2, y2. Location of where this cell is drawn
+	canvas : tkinter Canvas object
+		canvas on which cell will be drawn
+	bfactor : int 0-1
+		How "blue" the cell is. 1 is white, 0 is blue
+	rect : tkinter Canvas Rectangle object
+		objecting representing rectangle used to draw cell
+	color: hex int
+		rectangle's color as a hex value
+
+	Methods
+	-------
+	rgbToHex(red, green, blue)
+		input rgb value, returns its hex value
+	setColor(red, green,  blue)
+		changes the color of the rectangle 
+	"""
+	def __init__(self, canvas, x1, y1, x2, y2, bfactor):
+		self.canvas = canvas
 		self.x1 = x1
 		self.y1 = y1
 		self.x2 = x2
 		self.y2 = y2
-		self.color = self.rgbToHex(rgb[0], rgb[1], rgb[2])
+		a = int(255*(1-bfactor))
+		self.color = self.rgbToHex(a, a, 255)
+		self.rect = canvas.create_rectangle(x1, y1, x2, y2, fill=self.color, outline="")
+		canvas.pack()
 
 	def rgbToHex(self, red, green, blue):
+		""" input rgb value for a color and this function
+		will return its hex value """
 		return "#%02x%02x%02x" % (red, green, blue) 
+
+
+	def changeColor(self, bfactor):
+		a = int(255*(bfactor))
+		self.canvas.itemconfig(self.rect, fill=self.rgbToHex(a, a, 255))
--- ocean2.py
+++ ocean2.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from random import uniform
+import cell
 
 def rgbToHex(red, green, blue):
 	return "#%02x%02x%02x" % (red, green, blue) 
@@ -15,19 +16,21 @@ rows = int((HEIGHT - 2*MARGIN)/CELL_SIZE)
 
 window = tk.Tk()
 
-w = tk.Canvas(window, width=WIDTH, height=HEIGHT)
+canvas = tk.Canvas(window, width=WIDTH, height=HEIGHT)
+
+# initialize all the cells
+cells = []
 for i in range(cols):
 	for j in range(rows):
 		x1 = i*CELL_SIZE + MARGIN
 		y1 = j*CELL_SIZE + MARGIN
 		x2 = i*CELL_SIZE + CELL_SIZE + MARGIN
 		y2 = j*CELL_SIZE + CELL_SIZE + MARGIN
-
 		a = int(uniform(5,200))
+		cells.append(cell.Cell(canvas, x1, y1, x2, y2, uniform(80,100)/100))
 
-		color = rgbToHex(a, a, 200)
 
-		w.create_rectangle(x1, y1, x2, y2, fill=color, outline="")
-		w.pack()
-
-window.mainloop()
+while True:
+	cells[int(uniform(0,len(cells)))].changeColor(0.8)
+	window.update_idletasks()
+	window.update()
